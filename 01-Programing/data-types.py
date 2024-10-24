105e2cf725dc136b38d87d35001aa1b780e2a28c
SuryaKarmakar <suryakarmakar.development@gmail.com>
2024-10-25 01:21:16 +0530

    Numbers

--- 01-Programing/data-types.py
+++ 01-Programing/data-types.py
@@ -1,7 +1,8 @@
-# Strings: strings are immutable. once declared and assigned a value, they cannot be changed
+# 1. Strings: strings are immutable.
 str1 = 'hello'
 str2 = "world"
 str3 = "type's"
+print(type(str1))
 print("str1 =", str1, "str2 =", str2, "str3 =", str3)
 
 # use triple quotes for multiple line (it's a alternative of \n)
@@ -17,3 +18,26 @@ print(str1[1])
 
 # slice a substring
 print(str1[0:2])
+
+# 2. Numbers: numbers are immutable.
+# Integers
+i = 10 
+i2 = int(10) 
+print(type(i), type(2))
+print('i =', i, 'i2 =', i2)
+
+# Floating Point Numbers
+f = 10.00
+f2 = float(10.00)
+print(type(f), type(f2))
+print('f =', f, 'f2 =', f2)
+
+# Complex Numbers
+c = 7 + 4j
+print(type(c))
+print('c =', c)
+
+# extract only the imaginary or real part of the complex number
+c2 = 44.9 + 89e4j 
+print ('real =', c2.real) 
+print ('imaginary =', c2.imag)
