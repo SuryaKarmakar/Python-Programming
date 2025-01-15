9312a9c958508378cb147cd7b1afb99a17720942
SuryaKarmakar <suryakarmakar.development@gmail.com>
2025-01-15 18:07:36 +0530

    range funcation in loop

---
+++ 03-MCA-Programing/10-Range-Funcation.py
@@ -0,0 +1,17 @@
+# santax - for  <var> in range(<number>)
+
+# loop will run from 0 to 4 with increments of 1.
+for x in range(5):
+    print(x)
+
+# we can change the starting value 0 to any other.
+# loop will run from 2 to 4 with increments of 1.
+for x in range(2, 5):
+    print(x)
+
+# the increment value also can be change 1 to any other.
+# (start range, end range, increment by)
+# loop will run from 5 to 9 with increments of 2.
+for x in range(5, 10, 2):
+    print(x)
+
