f5300f0d806d172eb80254c78d3e4ac98650a074
SuryaKarmakar <suryakarmakar.development@gmail.com>
2025-02-28 00:06:55 +0530

    p8

---
+++ 04-MCA-Lab/08-Program.py
@@ -0,0 +1,12 @@
+# Write a python program to add new data item into list using append() and insert().
+
+# Initial list
+data_list = [10, 20, 30, 40, 50]
+
+# Using append() to add a new item at the end
+data_list.append(60)
+print(data_list)
+
+# Using insert() to add a new item at a specific position
+data_list.insert(2, 25)
+print(data_list)
