1238e2904edaab84d052c0ab8d586c6c3976a610
SuryaKarmakar <suryakarmakar.development@gmail.com>
2025-03-04 20:17:24 +0530

    update - while loop with else

--- 03-MCA-Programing/11_while_loop.py
+++ 03-MCA-Programing/11_while_loop.py
@@ -1,7 +1,14 @@
 # syntax - while <expression>
 
 x = 0
-
 while x < 10:
     print(x)
     x += 1
+
+# while loop with else statement. 
+y = 0
+while x < 10:
+    print(x)
+    x += 1
+else:
+    print("End of lopp")
