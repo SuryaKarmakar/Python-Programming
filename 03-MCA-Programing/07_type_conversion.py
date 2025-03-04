4b6bbcf5993c6a9f5dab5f85f88c10293734d961
SuryaKarmakar <suryakarmakar.development@gmail.com>
2025-03-04 18:15:55 +0530

    update - error handeling, bytes and json conversistion

--- 03-MCA-Programing/07_type_conversion.py
+++ 03-MCA-Programing/07_type_conversion.py
@@ -44,3 +44,28 @@ print('list_to_tuple =', list_to_tuple, type(list_to_tuple))
 # Converts x to dictionary
 tuple_to_dict = dict([(1, 'one'), (2, 'two')])
 print('tuple_to_dict =', tuple_to_dict, type(tuple_to_dict))
+
+# Use try-except  blocks to catch and handle exceptions that arise from failed type conversions.
+try: 
+    number = int(input("Enter a number: ")) 
+except ValueError: 
+    print("That's not a valid number!")
+
+# validate the data to ensure it meets the expected format or criteria.
+user_input = input("Enter a positive number: ") 
+if user_input.isdigit(): 
+    number = int(user_input) 
+else: 
+    print("Invalid input!")
+
+# Correctly converting string to bytes and back 
+original_string = "Hello, world!" 
+bytes_data = original_string.encode('utf-8')  # Encoding string to bytes 
+decoded_string = bytes_data.decode('utf-8')  # Decoding bytes back to string
+
+import json 
+# Converting a dictionary to a JSON string 
+data_dict = {'name': 'John', 'age': 30, 'city': 'New York'} 
+json_string = json.dumps(data_dict) 
+# Converting JSON string back to dictionary 
+decoded_dict = json.loads(json_string) 
