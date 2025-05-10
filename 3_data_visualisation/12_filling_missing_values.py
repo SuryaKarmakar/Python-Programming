165bd7865128975bc4a4ec0027353aee098d5b29
SuryaKarmakar <suryakarmakar.development@gmail.com>
2025-05-10 15:19:11 +0530

    filling missing values

---
+++ 3_data_visualisation/12_filling_missing_values.py
@@ -0,0 +1,22 @@
+# importing pandas and numpy
+import pandas as pd
+import numpy as np
+
+# dictionary of lists
+dict = {
+    "First Score": [100, 90, np.nan, 95],
+    "Second Score": [30, 45, 56, np.nan],
+    "Third Score": [np.nan, 40, 80, 98],
+}
+
+# creating a dataframe from dictionary
+df = pd.DataFrame(dict)
+
+# 1: Filling null values with a single value
+print(df.fillna(0))
+
+# 2: Filling null values with the previous ones
+print(df.fillna(method="ffill"))
+
+# 3: Filling null value with the next ones
+print(df.fillna(method="bfill"))
